e7b06ff07358f3f259682c0a376c23009d249482
Tales Hs <webtailss@gmail.com>
2024-10-25 00:24:49 -0300

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,13 @@ const nextConfig = {
             },
         ],
     },
+    env: {
+        NEXT_PUBLIC_FUSIONAUTH_BASE_URL: process.env.NEXT_PUBLIC_FUSIONAUTH_BASE_URL,
+        NEXT_PUBLIC_FUSIONAUTH_AUTH_TOKEN: process.env.NEXT_PUBLIC_FUSIONAUTH_AUTH_TOKEN,
+        NEXT_PUBLIC_FUSIONAUTH_APPLICATION_ID: process.env.NEXT_PUBLIC_FUSIONAUTH_APPLICATION_ID,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    },
 };
 
 export default nextConfig;
